dfcf2146904b793c0f0103ae87d2e465fa7f1978
comus3 <come.plantin.carrenard@gmail.com>
2023-12-08 14:17:26 +0100

    correction nomenclature

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
+import Tele from "./models/tele.js";
+
 const express = require('express');
 const app = express();
 app.use(express.static("public"));
-
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));  
+app.post('/',async function (request,response){
 
-
-app.use(express.static('public'));
-
-app.post('/', (request,response)=>{
-
-    //do something
+    if (request.body.something != null){
+        //do something
+        const test = await Tele.loadMany();
+    }
 })
-
-
-app.get('/',(request,response)=>{
-    //do something
+app.get('/',async function (request,response){
+    if (request.query.something != null){
+        //do something
+    }
 })
-
-
 app.listen(3000, function(){
     console.log("Server ok");
 });
