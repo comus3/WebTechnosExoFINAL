c5a383d8fd03968bda36be534776789dcfec5f53
comus3 <come.plantin.carrenard@gmail.com>
2023-12-08 14:50:33 +0100

    update package

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 import Tele from "./models/tele.js";
+import express from "express"
 
-const express = require('express');
+let televisionList = new Tele;
 const app = express();
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));  
-app.post('/',async function (request,response){
-
+app.post('/add-tv',async function (request,response){
     if (request.body.something != null){
+        
+        response.redirect('/');
+    }
+})
+app.get('/buy-tv',async function (request,response){
+    if (request.query.something != null){
         //do something
-        const test = await Tele.loadMany();
+        response.redirect('/');
+
+    }
+})
+app.get('/complete-cause',async function (request,response){
+    if (request.query.something != null){
+        //do something
+        response.redirect('/');
+
     }
 })
 app.get('/',async function (request,response){
     if (request.query.something != null){
-        //do something
+        //render page
+        const wishList = await televisionList.loadMany({etat:0});
+        const baughtList = await televisionList.loadMany({etat:1,etat:2})
+        res.render('home.ejs', {wishList,baughtList});
+        response.redirect('/');
+
     }
 })
 app.listen(3000, function(){
